3ee2fd09ce00f7051134c9f128b955389869f91b
RazvanMacovei10 <macoveirazvan55@yahoo.com>
2023-03-27 12:47:45 +0300

    Added Services and Data layers and created first migration of database

---
+++ TheatreAPI/DataLayer/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
